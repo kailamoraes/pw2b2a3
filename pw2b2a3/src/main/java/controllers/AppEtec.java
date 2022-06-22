9e0ebc452cb5ca6304a8037767bcae1d6ca8c74e
kailamoraes <kailaleontinademoraes@gmail.com>
2022-06-22 18:00:16 +0000

    Avaliação

---
+++ pw2b2a3/src/main/java/controllers/AppEtec.java
@@ -0,0 +1,35 @@
+package etec.palmital.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class NumerosController {
+    @Value("${aluno}")
+    private String nome;
+    }
+} 
+
+@RequestMapping("/")
+public String calc(Model model) {
+    int[] resultado = null;
+    for(int val = 2 ; val < 30 ; val++) {
+        if (val % 2 == 0){
+            if(resultado == null) {
+                resultado = new int[1];
+            } else {
+                int[] temp = resultado;
+                resultado = new int[temp.length + 1];
+                for(int pos = 0;pos < temp.legth;pos++)
+                resultado[pos] = temp[pos];
+            }
+            resultado[resultado.length - 1] = val;
+        }
+    }
+    model.addAtribute("valores", resultado);
+    model.addAtribute("aluno", nome);
+    return "/numeros/sequencia";
+}
+}
---
+++ pw2b2a3/src/main/java/controllers/NumerosController.java
@@ -0,0 +1,11 @@
+package etec.palmital;
+
+import org.springframework.boot.SpringApllication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEtec {
+    public static void main(String[] args) {
+        SpringApplication.run(AppEtec.class, args);
+    }
+}
